2b3ff8142bf6150113a8831d95430aaeca8840d0
rajkumar <raj@rajhomepc>
2017-02-09 17:34:49 +0530

    update

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var port = process.env.PORT || 3000;
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname + 'views'));
 
-//app.use(express.static(__dirname + '/static')); 
+/* //app.use(express.static(__dirname + '/static'));  */
+
 
 app.get('/', function(req, res){
     res.render('index');
