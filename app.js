726505584ffa8653de711a28773667d10feb70b4
rajkumar <raj@rajhomepc>
2017-02-09 17:09:50 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 
 var express = require('express'); 
+var path = require('path');
+
 var app = express();
 var port = process.env.PORT || 3000; 
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname + 'views'));
+
 app.use(express.static(__dirname + '/static')); 
 
-app.listen(port)
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.listen(port);
 
 
 
